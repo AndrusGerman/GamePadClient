12bcdc8c5c8aba3d756cbf6c1be2d83f3de8cc24
AndrusGerman <andrusdiazaleman@gmail.com>
2022-05-26 03:35:37 -0400

    create set and get data in string mode

--- lib/gamepad/repository/models/buttonViewScreen.dart
+++ lib/gamepad/repository/models/buttonViewScreen.dart
@@ -3,11 +3,11 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 
 class ButtonViewScreen {
-  late Offset position;
-  late double size;
+  late Offset position = Offset.zero;
+  late double size = 0;
   late ButtonViewScreenType type;
-  late int id;
-  late List<String> codes;
+  late int id = 0;
+  late List<String> codes = [];
 
   ButtonViewScreen setData(Offset position, double size,
       ButtonViewScreenType type, List<dynamic> codes) {
@@ -18,7 +18,29 @@ class ButtonViewScreen {
     return this;
   }
 
-  asJsonString() {}
+  String asJsonString() {
+    final map = {
+      'position': {'x': position.dx, 'y': position.dy},
+      'size': size,
+      'type': type.index,
+      'id': id,
+      'codes': codes,
+    };
+
+    return jsonEncode(map);
+  }
+
+  setDataByJSON(String data) {
+    final Map<String, dynamic> mapa = jsonDecode(data);
+    final Map<String, dynamic> positionRaw = mapa['position'];
+
+    position = Offset(positionRaw['x'] as double, positionRaw['y'] as double);
+    size = mapa['size'] as double;
+    final rawType = mapa['type'] as int;
+    type = ButtonViewScreenType.values[rawType];
+    id = mapa['id'] as int;
+    codes = mapa['codes'] as List<String>;
+  }
 }
 
 enum ButtonViewScreenType {
