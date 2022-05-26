13193526f295abd7b68fd16b45678a5ea6530892
AndrusGerman <andrusdiazaleman@gmail.com>
2022-05-26 16:56:46 -0400

    remove button mouse add

--- lib/gamepad/ui/widgets/buttons/button_mouse.dart
+++ lib/gamepad/ui/widgets/buttons/button_mouse.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:game_pad_client/gamepad/bloc/GamePadAddButtonPosition.dart';
+import 'package:game_pad_client/gamepad/bloc/GamePadModeBloc.dart';
 import 'package:game_pad_client/gamepad/repository/models/buttonViewScreen.dart';
 import 'package:game_pad_client/gamepad/repository/connect_ws.dart';
 import 'package:game_pad_client/gamepad/ui/widgets/buttons/circle_button_raw.dart';
@@ -11,6 +14,21 @@ class ButtonSimpleMouseGamePad extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final st = BlocProvider.of<GamePadModeCubit>(context);
+
+    return StreamBuilder(
+        builder: (context, AsyncSnapshot<GamePadModeIndex?> value) {
+          if (GamePadModeIndex.values[value.data!.index] ==
+              GamePadModeIndex.removeButtonsMode) {
+            return createBtnRemove(context);
+          }
+          return createBtn(context);
+        },
+        initialData: GamePadModeIndex.playMode,
+        stream: st.stream);
+  }
+
+  createBtn(BuildContext context) {
     final bloc = gbloc.BlocProvider.of<ConnectionWS>(context);
 
     return CircleButtonRaw(
@@ -25,6 +43,19 @@ class ButtonSimpleMouseGamePad extends StatelessWidget {
       },
       onTapCancel: () {
         bloc.sendSignal(EventWSCreator(3, 3, Value: buttonData.codes[0]));
+      },
+      color: Color.fromARGB(255, 82, 230, 185),
+    );
+  }
+
+  createBtnRemove(BuildContext context) {
+    final gpab = gbloc.BlocProvider.of<GamePadAddButtonPositionBloc>(context);
+
+    return CircleButtonRaw(
+      buttonData: buttonData,
+      // Iniciar
+      onTapUp: (details) {
+        gpab.removeBtn(buttonData.id);
       },
       color: Color.fromARGB(255, 82, 230, 185),
     );
